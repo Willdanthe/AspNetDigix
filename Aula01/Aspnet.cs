eca42e597e08b2696dc1814fe14c24e3ee371942
willdanthe <will.allaman@hotmail.com>
2025-03-21 16:58:34 -0400

    Currículo com Aspnet

--- Aula01/Aspnet.cs
+++ Aula01/Aspnet.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 
 using System.Security.Cryptography;
-
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Hosting;
+using System.Net;
 
 namespace Aula01
 {
@@ -14,15 +15,19 @@ namespace Aula01
     {
         static void Main(string[] args)
         {
+            var Builder = WebApplication.CreateBuilder(args);
+            var app = Builder.Build();
             
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseEndpoints(endpoints => {
+                endpoints.MapGet("/", async context =>{
+                    context.Response.Redirect("/index.html");
+                });
+            });
+
+            app.Run();
         }
 
-        public static HostBuilder CreateHostBuilder(string[] args) =>
-        { // Define o método que é responsável por criar o host da aplicação 
-            Host.CreateDefaultBuilder(args) // Cria o shost com configuração padrão do ambiente
-            .ConfigureWebHostDefault(web);
-            
-
-        }
     }
 }
