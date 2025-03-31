e953bd974beea377349e3f7375599bfc64b81401
willdanthe <will.allaman@hotmail.com>
2025-03-31 13:36:20 -0400

    Atividade Finalizada API

--- Aula03_Exemplo4/wwwroot/script.js
+++ Aula03_Exemplo4/wwwroot/script.js
@@ -20,11 +20,23 @@ function carregarUsuarios() {
                     <tr>
                         <td>${usuario.id}</td>
                         <td>${usuario.nome}</td>
+                        <td>${usuario.password}</td>
                         <td>${usuario.ramal}</td>
                         <td>${usuario.especialidade}</td>
                         <td >
-                            <button class="btn btn-secondary" onclick="editarUsuario(${usuario.id})">Editar</button>
-                            <button class="btn btn-danger" onclick="deletarUsuario(${usuario.id})">Deletar</button>
+                        
+                            <button class="btn btn-secondary" onclick="editarUsuario(${usuario.id})">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+  <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+  <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
+</svg>
+                            </button>
+                            <button class="btn btn-danger" onclick="deletarUsuario(${usuario.id})">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+  <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+</svg>
+                            </button>
                         </td>
                     </tr>
                     `;
@@ -36,7 +48,7 @@ function carregarUsuarios() {
 
 function salvarUsuario(event) {
     event.preventDefault(); // Previne o comportamento padrão do formulário (não envia e recarrega a página)
-    
+
     const id = document.getElementById("idUsuario").value; // Obtém o valor do ID
     const usuario = {
         id: parseInt(id || 0), // Se o ID estiver vazio, define 0
@@ -46,7 +58,7 @@ function salvarUsuario(event) {
         especialidade: document.getElementById("especialidade").value
     };
     console.table(usuario)
-    
+
     // Verifica se o ID já existe
     fetch(`${API}/${usuario.id}`)
         .then(res => {
--- Aula03_Exemplo4/wwwroot/scriptSoftware.js
+++ Aula03_Exemplo4/wwwroot/scriptSoftware.js
@@ -24,8 +24,18 @@ function carregarSoftwares() {
                         <td>${software.memoria_Ram}</td>
                         <td>${software.fk_Maquina}</td>
                         <td >
-                            <button class="btn btn-secondary" onclick="editarSoftware(${software.id})">Editar</button>
-                            <button class="btn btn-danger" onclick="deletarSoftware(${software.id})">Deletar</button>
+                            <button class="btn btn-secondary" onclick="editarSoftware(${software.id})">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+  <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+  <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
+</svg>
+                            </button>
+                            <button class="btn btn-danger" onclick="deletarSoftware(${software.id})">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+  <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+</svg>
+                            </button>
                         </td>
                     </tr>
                     `;
@@ -37,7 +47,7 @@ function carregarSoftwares() {
 
 function salvarSoftware(event) {
     event.preventDefault(); // Previne o comportamento padrão do formulário (não envia e recarrega a página)
-    
+
     const id = document.getElementById("idSoftware").value; // Obtém o valor do ID
     const software = {
         id: parseInt(id || 0), // Se o ID estiver vazio, define 0
@@ -47,7 +57,7 @@ function salvarSoftware(event) {
         fk_Maquina: document.getElementById("fk_Maquina").value
     };
     console.table(software)
-    
+
     // Verifica se o ID já existe
     fetch(`${API}/${software.id}`)
         .then(res => {
@@ -75,7 +85,7 @@ function salvarSoftware(event) {
         })
         .then(res => res.json()) // Converte a resposta para JSON
         .then(() => {
-            document.getElementById("machineForm").reset(); // Reseta o formulário
+            document.getElementById("softwareForm").reset(); // Reseta o formulário
             carregarSoftwares(); // Atualiza a lista de usuários
         })
         .catch(err => console.error("Erro:", err));
@@ -86,7 +96,7 @@ function editarSoftware(id) {
     fetch(`${API}/${id}`)
         .then(res => res.json())
         .then(software => {
-            document.getElementById("idMaquina").value = software.id;
+            document.getElementById("idSoftware").value = software.id;
             document.getElementById("produto").value = software.produto;
             document.getElementById("harddisk").value = software.hardDisk;
             document.getElementById("memoria_ram").value = software.memoria_Ram;
